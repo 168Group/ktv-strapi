014125a18922f63d08491b397ff0652850f98241
Sebastian Maj <officialwolftech@gmail.com>
2026-07-02 00:05:27 -0400

    Switch media uploads to Cloudflare R2 (S3 provider)
    - Configure upload plugin to use @strapi/provider-upload-aws-s3 pointed at R2
      (endpoint/bucket/credentials/public URL from Railway env vars).
    - Allow the R2 public URL in the admin CSP so media previews render.

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,21 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    // Allow the admin panel to load media previews served from Cloudflare R2.
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', env('R2_PUBLIC_URL')],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', env('R2_PUBLIC_URL')],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -6,9 +6,26 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
-      provider: "local",
+      // Cloudflare R2 (S3-compatible). Uploaded media is stored in R2 and
+      // served from the public R2 URL. Credentials come from Railway env vars.
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("R2_PUBLIC_URL"),
+        s3Options: {
+          endpoint: env("R2_ENDPOINT"),
+          region: "auto",
+          credentials: {
+            accessKeyId: env("R2_ACCESS_KEY_ID"),
+            secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+          },
+          params: {
+            Bucket: env("R2_BUCKET"),
+          },
+        },
+      },
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
